6b709c143bb86e856bea1d86267dc05d4e61d750
acp21h <acpohl21@gmail.com>
2020-02-12 01:03:35 -0500

    Worked on basic GUI work

--- main.py
+++ main.py
@@ -4,14 +4,38 @@ import re
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import os
+from tkinter import *
 
 
+# Setup function gets mapper name from user
+# Starts web driver
+# Begins to parse the first page of songs
 def setup():
-    mapper = input("Input mapper name:").lower()
-    driver = webdriver.Chrome()
-    parsePage(mapper, 1, driver)
 
 
+
+    # Starting web driver
+    option = webdriver.ChromeOptions()
+    option.add_argument("headless")
+    driver = webdriver.Chrome(options=option)
+    print("Starting web driver")
+
+    root = Tk()
+    root.geometry("500x200")
+    gui = GUI(root, driver)
+    root.mainloop()
+
+    # Download maps
+    #mapper = gui.getName()
+    #mapper = input("Input mapper name:").lower()
+    #parsePage(mapper, 1, driver)
+
+
+# parsePage() takes a mapper name, a page number, and a webdriver as paramaters
+# The function then loops through the page looking for html links that match specific criteria
+# It saves these links in an array and then passes them all to DLSongs()
+# parsePage() then recursively calls itself with page + 1
+# If page does not exist, program stops
 def parsePage(mapper : str, page : int, driver : webdriver):
     if page == 1:
         url = "https://bsaber.com/songs/new/?uploaded_by="
@@ -29,13 +53,15 @@ def parsePage(mapper : str, page : int, driver : webdriver):
             else:
                 print("All Downloads Complete")
                 exit()
+        # Get all links containing "https://bsaber.com/songs/"
         songs = soup.findAll("a", {"rel" : False}, href=re.compile("https://bsaber.com/songs/"))
         i = 0
+        # Create lists to store urls and song names
         urls = []
         names = []
         for item in songs:
             dl = (str(item))
-            # If statement makes sure only authors songs are captured
+            # If statement makes sure only author's songs are captured
             try:
                 fields = dl.split('"')
                 # Skip over first unneeded link
@@ -44,15 +70,19 @@ def parsePage(mapper : str, page : int, driver : webdriver):
                         break
                     urls.append(fields[1])  # Add songs to array
                     names.append(fields[3])
+            # Catch all index errors thrown by incorrect link captures
             except IndexError:
                 print("Invalid Song")
             i += 1
-        print(urls)
-        print(names)
+        # Debug print statements below
+        # print(urls)
+        # print(names)
         DLSongs(urls, names)
         print("Finished Page")
+        # Recursively loop through function until empty page found
         parsePage(mapper, page + 1, driver)
-    except ImportError:
+    # This exception is currently unused, should be removed
+    except Exception:
         if page == 1:
             print("Mapper does not exist")
 
@@ -62,10 +92,11 @@ def main():
 
 
 def DLSongs(urls: str, names: str):
-    print("URLS")
-    print(urls)
-    print("NAMES")
-    print(names)
+    #
+    # print("URLS")
+    # print(urls)
+    # print("NAMES")
+    # print(names)
     for i in range(len(urls)):
         urlsplit = urls[i].split("/")
         key = urlsplit[4]
@@ -73,13 +104,13 @@ def DLSongs(urls: str, names: str):
         r = requests.get(key, stream=True)
         print("Dowloading ", names[i])
         print(urls[i])
-        exsist = os.path.isdir("dl")
-        print(exsist)
-        if exsist == False:
+        # Check if dl folder exists in working directory
+        direct = os.path.isdir("dl")
+        if not direct:  # If dl does not exist, create dl
             os.mkdir("dl")
-        if r.status_code == 200:
-            name = fixName(names[i])
-            directory = os.getcwd()
+        if r.status_code == 200:  # If request successful
+            name = fixName(names[i])  # Use fixName() to fix song name
+            directory = os.getcwd()   # Get working directory
             directory = directory + r"/dl//"
             path = directory + name + ".zip"
             print("Saving to ", path)
@@ -87,7 +118,8 @@ def DLSongs(urls: str, names: str):
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
 
-
+# fixName() accepts a string and removes certain characters that cause problems
+# in Windows file names
 def fixName(name: str):
     name = name.replace(r"\\", "")
     name = name.replace("/", "")
@@ -100,6 +132,30 @@ def fixName(name: str):
     name = name.replace('|', "")
     return name
 
+class GUI:
+
+    def __init__(self, master, driver):
+        frame = Frame(master)
+        frame.pack()
+        self.driver = driver
+        self.label = Label(frame, text="Input Mapper Name")
+        self.label.pack()
+        self.button = Button(frame, text="DOWNLOAD!", fg="red", command=self.beginDL)
+        self.button.pack()
+        self.input = Entry(frame)
+        self.input.pack()
+
+    def beginDL(self):
+        mapper = self.input.get()
+        parsePage(mapper, 1, self.driver)
+
+
+    def begin(self):
+        root = Tk()
+        root.geometry("500x200")
+        gui = GUI(root)
+        root.mainloop()
+
+
 
 main()
-print(os.getcwd())
